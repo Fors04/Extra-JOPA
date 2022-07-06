b553e5495907cd6323fa70eaa049245d3602332a
MrCobraX <mrcobra@yandex.ru>
2022-07-06 18:37:42 +0300

    5435345

--- src/main/java/extra_jopa/item/ItemFew.java
+++ src/main/java/extra_jopa/item/ItemFew.java
@@ -42,7 +42,7 @@ public class ItemFew extends ElementsExtraJOPA.ModElement {
 					multimap.put(SharedMonsterAttributes.ATTACK_DAMAGE.getName(),
 							new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "Weapon modifier", (double) this.getAttackDamage(), 0));
 					multimap.put(SharedMonsterAttributes.ATTACK_SPEED.getName(),
-							new AttributeModifier(ATTACK_SPEED_MODIFIER, "Weapon modifier", -3, 0));
+							new AttributeModifier(ATTACK_SPEED_MODIFIER, "Weapon modifier", 12, 0));
 				}
 				return multimap;
 			}
